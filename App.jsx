3419c2199ccf4140c6ef07a6b07ac598c775036b
Tarek Habash <tarek.soliman797@gmail.com>
2022-10-31 23:08:18 +0100

    first commit

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom"
+import Header from "./components/Header";
+import Cart from "./pages/Cart";
+import Photos from "./pages/Photos";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route 
+          exact path="/"
+          element = {<Photos />}
+        />
+
+        <Route 
+          exact path="/cart"
+          element={<Cart />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
