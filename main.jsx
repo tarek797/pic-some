bd3a355b52b15e9441d77c4260be28268aee0c08
Tarek Habash <tarek.soliman797@gmail.com>
2022-11-03 16:27:59 +0100

    Second commit: using context to hold the data that will be consumed by different components

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import {BrowserRouter as Router} from "react-router-dom"
-
+import { ContextProvider } from "./Context"
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Router>
-  <App />
-</Router>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+<ContextProvider>
+  <Router>
+      <App />
+  </Router>
+</ContextProvider>
+)
